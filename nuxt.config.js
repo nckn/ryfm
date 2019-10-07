feb39d59da1a4146b3cb08c791d463d2f5b9f25c
Niels Christian Konrad Nielsen <mail@nckn.dk>
2019-10-07 08:09:12 +0200

    mode: 'universal'

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,6 +21,7 @@ module.exports = {
       // { type: 'text/javascript', src: 'https://wchat.freshchat.com/js/widget.js' }
     ]
   },
+  mode: 'universal',
   server: {
     port: 8000, // default: 3000
     host: '0.0.0.0', // default: localhost
