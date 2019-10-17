37a7d736c7fb9695ea836273c997e32f4bb25778
Niels Christian Konrad Nielsen <mail@nckn.dk>
2019-10-17 16:46:41 +0200

    Still trying to generate correctly

--- .nuxt/utils.js
+++ .nuxt/utils.js
@@ -137,7 +137,7 @@ export async function setContext(app, context) {
 
       payload: context.payload,
       error: context.error,
-      base: '/dist/',
+      base: '/ryfm/',
       env: {}
     }
     // Only set once
