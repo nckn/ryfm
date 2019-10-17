050f68e9945e25406ea29549a6cbd12ee2431719
Niels Christian Konrad Nielsen <mail@nckn.dk>
2019-10-17 16:05:31 +0200

    New dist after edits in nuxt.config.js

--- .nuxt/server.js
+++ .nuxt/server.js
@@ -26,7 +26,7 @@ const createNext = ssrContext => (opts) => {
   }
   opts.query = stringify(opts.query)
   opts.path = opts.path + (opts.query ? '?' + opts.query : '')
-  const routerBase = '/'
+  const routerBase = '/ryfm/'
   if (!opts.path.startsWith('http') && (routerBase !== '/' && !opts.path.startsWith(routerBase))) {
     opts.path = urlJoin(routerBase, opts.path)
   }
--- .nuxt/utils.js
+++ .nuxt/utils.js
@@ -137,7 +137,7 @@ export async function setContext(app, context) {
 
       payload: context.payload,
       error: context.error,
-      base: '/',
+      base: '/ryfm/',
       env: {}
     }
     // Only set once
