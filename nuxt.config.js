2d8b1adabdad05931c34c8bfa73bbbee12b6e0a5
Niels Christian Konrad Nielsen <mail@nckn.dk>
2019-10-17 08:46:36 +0200

    Went back to normal name and title. Now know that deploy works

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,7 +5,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'RYYYYYYYFM',
+    title: 'RYFM',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -37,6 +37,5 @@ module.exports = {
         'primary-color':                '#479ff1'
       }
     }
-  },
-
+  }
 }
