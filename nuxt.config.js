95386b4c84e504516cba0ab2805a90261eedf02f
Niels Christian Konrad Nielsen <mail@nckn.dk>
2019-10-19 11:37:50 +0200

    2.1

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'RYFM test 2.0 - _nuxt back',
+    title: 'RYFM test 2.1',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
