43ab02a4cf08afa55d58309949551fd3a0866a6f
Niels Christian Konrad Nielsen <mail@nckn.dk>
2019-10-19 12:20:10 +0200

    Making responsive styling of footer

--- nuxt.config.js
+++ nuxt.config.js
@@ -39,7 +39,8 @@ module.exports = {
     }
   },
   router: {
-    base,
+    base: '/ryfm/'
+    // base,
   },
   plugins: [
     // {src: '~/plugins/ant-design-vue', ssr: false}
--- pages/index.vue
+++ pages/index.vue
@@ -21,6 +21,7 @@
           div.cell-row(v-for="(drum, index) in drums")
             Cell(v-for="(cell, index) in sequenceCells[index]" :class_name="'sixteen-buttons'" v-bind:id="index" :key="index")
     .footer
+      .trigger-footer
       .control-row.one
         Slider(:slider_name="'Kick'" :min="30" :max="500" :value="50" :step="1")
         Slider(:slider_name="'Snare'" :min="100" :max="4096" :value="4096" :step="1")
