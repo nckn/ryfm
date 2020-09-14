d8a1a2d9180237639e6f06e78c8eb59151e41cb9
Niels Christian Konrad Nielsen <mail@nckn.dk>
2020-09-14 13:36:25 +0200

    changing to universal mode again

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,7 +23,8 @@ module.exports = {
       // { type: 'text/javascript', src: 'https://wchat.freshchat.com/js/widget.js' }
     ]
   },
-  ssr: false,
+  mode: 'universal',
+  // ssr: false,
   server: {
     port: 8000, // default: 3000
     host: '0.0.0.0', // default: localhost
